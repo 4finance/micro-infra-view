0f9db66d7f5e55025605d232b7cccba871e222a8
Piotr Pietrzak <piotr.pietrzak@4finance.com>
2015-08-24 15:04:07 +0200

    Collaborators url fixed to work with production URLs because Restangular starts from root instead of relative path

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,12 +2,10 @@
 
 angular.module('microInfraView')
   .controller('MainCtrl', function ($scope, $log, Restangular) {
-    Restangular.one('all').get().then( function(collaborators) {
+    Restangular.all('collaborators').one('all').get().then( function(collaborators) {
       $scope.current = {collaborator:{}};
       $scope.allCollaborators = collaborators.plain();
       $scope.parsedResponse = parseResponse($scope.allCollaborators);
       draw($scope.parsedResponse);
     });
-
-
   });
