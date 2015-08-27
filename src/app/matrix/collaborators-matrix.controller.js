fcb7b020561bd04cf39bb6fb3411b0c8b4effc86
Piotr Pietrzak <piotr.pietrzak@4finance.com>
2015-08-27 09:48:07 +0200

    Collaborators url fixed to work with production URLs because Restangular starts from root instead of relative path - the same for a second time because of forgotten matrix controller

--- src/app/matrix/collaborators-matrix.controller.js
+++ src/app/matrix/collaborators-matrix.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('microInfraView')
   .controller('CollaboratorsMatrixCtrl', function ($scope, $log, Restangular) {
-    Restangular.one('all').get().then(function (collaborators) {
+    Restangular.all('collaborators').one('all').get().then(function (collaborators) {
       $scope.current = {collaborator: {}};
 
       $scope.allCollaborators = collaborators.plain();
